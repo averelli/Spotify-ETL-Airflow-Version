2f646804ab8ebab59bd6c40c2a1f5735722922c0
Averelli <beloglazov.roman06@gmail.com>
2025-08-16 11:36:27 +0300

    chore: remove the spliting function

--- scripts/data_extraction/spotify_items_staging.py
+++ scripts/data_extraction/spotify_items_staging.py
@@ -36,22 +36,6 @@ def get_new_items(entity_type: str):
         return new_items
 
 
-def split_into_batches(items:list, batch_size:int=50) -> list:
-    """
-    Splits a list of URIs into batches of a specified size
-
-    Args:
-        items (list): The list to split
-        batch_size (int): The size of each batch
-
-    Returns:
-        list: A list of batches, where each batch is a list of items
-    """
-    logger = LoggingMixin().log
-    logger.info(f"Splitting {len(items)} items into batches of size {batch_size}")
-    return [items[i:i + batch_size] for i in range(0, len(items), batch_size)] if items else []
-
-
 def process_spotify_batch(batch: list, item_type:str, retry_limit:int = 2) -> dict:
         """
         Process a single batch of tracks or episodes.
