4b4510a432e859507c8c1ea29383e36f5f2964cb
Averelli <beloglazov.roman06@gmail.com>
2025-08-18 14:04:29 +0300

    feat: add cleanup staging, logging, move db and logger definition to dag

--- dags/main_dag.py
+++ dags/main_dag.py
@@ -1,11 +1,12 @@
 from airflow import DAG
-from airflow.decorators import task, short_circuit
+from airflow.decorators import task
 from airflow.utils.task_group import TaskGroup
 from airflow.operators.empty import EmptyOperator
 from airflow.exceptions import AirflowSkipException
-from scripts.data_extraction.raw_data_extractor import *
-from scripts.data_extraction.spotify_items_staging import *
-from scripts.data_transformation.transformer import *
+from airflow.utils.log.logging_mixin import LoggingMixin
+from scripts.data_extraction import raw_data_extractor as extractor
+from scripts.data_extraction import spotify_items_staging as staging
+from scripts.data_transformation import transformer
 from scripts.utils import split_into_batches
 from hooks.db_postgres_hook import PgConnectHook
 from hooks.spotify_api_hook import SpotifyClientHook
@@ -14,7 +15,9 @@ def create_staging_task_groups(item:str, task_group:str):
     with TaskGroup(f"stage_{item}s") as stage_item:
         @task
         def get_new_items_task():
-            return get_new_items(item)
+            logger = LoggingMixin().log
+            db = PgConnectHook()
+            return staging.get_new_items(db, logger, item)
 
         @task
         def split_into_staging_batches_task(items):
@@ -23,7 +26,10 @@ def create_staging_task_groups(item:str, task_group:str):
         
         @task
         def process_batch_task(batch):
-            return process_spotify_batch(batch, item_type=item)
+            logger = LoggingMixin().log
+            db = PgConnectHook()
+            sp_hook = SpotifyClientHook()
+            return staging.process_spotify_batch(db, logger, sp_hook, batch, item_type=item)
         
         @task()
         def retry_items_task(**context):
@@ -32,17 +38,22 @@ def create_staging_task_groups(item:str, task_group:str):
             for batch in batch_results:
                 if batch["processed_items"] == 0:
                     failed_items.extend(batch["failed_items"])
+
+            logger = LoggingMixin().log
+            db = PgConnectHook()
+            sp_hook = SpotifyClientHook()
             
-            return retry_items(failed_items, item_type=item)
+            return staging.retry_items(db, logger, sp_hook, failed_items, item_type=item)
         
         @task(trigger_rule="none_failed_min_one_success")
         def item_group_final_log_task(**context):
             batch_results = context["ti"].xcom_pull(task_ids=f"spotify_items_staging.{task_group}.stage_{item}s.process_batch_task")
             retry_results = context["ti"].xcom_pull(task_ids=f"spotify_items_staging.{task_group}.stage_{item}s.retry_items_task") or {}
-            return item_group_final_log(extraction_stats=batch_results, retry_stats=retry_results, item_type=item)
+            logger = LoggingMixin().log
+            return staging.item_group_final_log(logger, extraction_stats=batch_results, retry_stats=retry_results, item_type=item)
         
         @task.branch
-        def check_errors_task(batch_results, **context):
+        def check_errors_task(batch_results):
             failed_items = []
 
             for batch in batch_results:
@@ -73,7 +84,7 @@ def load_dims_task_groups(item_type:str):
         def get_staged_items_task():
             db = PgConnectHook()
             logger = LoggingMixin().log
-            staged_items = get_staged_items(db, logger, item_type)
+            staged_items = transformer.get_staged_items(db, logger, item_type)
             return staged_items
 
         @task 
@@ -85,7 +96,7 @@ def load_dims_task_groups(item_type:str):
         def core_batch_task(batch):
             db = PgConnectHook()
             logger = LoggingMixin().log
-            batch_time, batch_count = core_batch(db, logger, item_type, batch)
+            batch_time, batch_count = transformer.core_batch(db, logger, item_type, batch)
             return batch_time, batch_count
         
         staged_items = get_staged_items_task()
@@ -102,19 +113,23 @@ with DAG("spotify_etl_dag", schedule_interval=None, catchup=False):
     with TaskGroup("raw_files_extraction") as raw_files_extraction:
         @task
         def get_files_list_task():
-            return get_raw_files_list("/opt/airflow/raw_data")
+            return extractor.get_raw_files_list("/opt/airflow/raw_data")
         
         @task
         def get_max_ts_task():
-            return get_max_ts()
+            db = PgConnectHook()
+            return extractor.get_max_ts(db)
         
         @task
         def extract_raw_data_task(file_name, max_ts):
-            return extract_streaming_history(file_name=file_name, max_ts=max_ts)
+            db = PgConnectHook()
+            logger = LoggingMixin().log
+            return extractor.extract_streaming_history(db, logger, file_name=file_name, max_ts=max_ts)
         
-        @task 
+        @task
         def extraction_final_log_task(extraction_stats):
-            return extraction_final_log(extraction_stats)
+            logger = LoggingMixin().log
+            return extractor.extraction_final_log(logger, extraction_stats)
             
         files_list = get_files_list_task()
         max_ts = get_max_ts_task()
@@ -124,8 +139,9 @@ with DAG("spotify_etl_dag", schedule_interval=None, catchup=False):
         [files_list, max_ts] >> extraction_stats >> final_log
 
     @task.branch
-    def skip_staging_tasks(raw_extraction_status):
-        if raw_extraction_status == "Skip downstream tasks":
+    def skip_staging_tasks(final_log):
+        raw_extraction_status = final_log["message"]
+        if raw_extraction_status != "Skip downstream":
             return "run_spotify_items_staging"
         else:
             return "run_transformation"
@@ -148,7 +164,7 @@ with DAG("spotify_etl_dag", schedule_interval=None, catchup=False):
         def run_second_group_task(**context):
             raw_extraction_status = context["ti"].xcom_pull(task_ids="raw_files_extraction.extraction_final_log_task")
             if raw_extraction_status != "Skip downstream":
-                return "spotify_items_staging.run_stage_artists_and_podcasts"
+                return "spotify_items_staging.dummy_success"
             else:
                 raise AirflowSkipException("Skipping artists and podcasts staging as raw extraction status is 'Skip downstream tasks'")
         run_second_group_check = run_second_group_task()
@@ -157,13 +173,36 @@ with DAG("spotify_etl_dag", schedule_interval=None, catchup=False):
         with TaskGroup("stage_artists_and_podcasts") as stage_artists_and_podcasts:
             for item in SPOTIFY_ITEM_TYPES[2:]: # Then stage artists and podcasts
                 create_staging_task_groups(item, "stage_artists_and_podcasts")
-        
-        stage_tracks_and_episodes >> run_second_group_check >> run_stage_artists_and_podcasts >> stage_artists_and_podcasts
 
+        @task
+        def log_staging_stats(**context):
+            logger = LoggingMixin().log
+            ti = context["ti"]
+            file_extraction_stats = ti.xcom_pull(task_ids="raw_files_extraction.extraction_final_log_task")
+            track_stats = ti.xcom_pull(task_ids="spotify_items_staging.stage_tracks_and_episodes.stage_tracks.item_group_final_log_task")
+            episode_stats = ti.xcom_pull(task_ids="spotify_items_staging.stage_tracks_and_episodes.stage_episodes.item_group_final_log_task")
+            artist_stats = ti.xcom_pull(task_ids="spotify_items_staging.stage_artists_and_podcasts.stage_artists.item_group_final_log_task")
+            podcast_stats = ti.xcom_pull(task_ids="spotify_items_staging.stage_artists_and_podcasts.stage_podcasts.item_group_final_log_task")
+            total_time = file_extraction_stats["total_time"] + track_stats["total_time"] + episode_stats["total_time"] + artist_stats["total_time"] + podcast_stats["total_time"]
+            logger.info(f"Staging completed in {total_time} seconds: Raw files: {file_extraction_stats["total_time"]} Tracks: {track_stats}, Episodes: {episode_stats}, Artists: {artist_stats}, Podcasts: {podcast_stats}")
+            return {
+                "raw_extraction_time": file_extraction_stats["total_time"],
+                "track_stats": track_stats,
+                "episode_stats": episode_stats,
+                "artist_stats": artist_stats,
+                "podcast_stats": podcast_stats,
+                "total_time": total_time
+            }
+        staging_stats= log_staging_stats()    
+        
+        stage_tracks_and_episodes >> run_second_group_check >> run_stage_artists_and_podcasts >> stage_artists_and_podcasts >> staging_stats
+
+    # Data transformation tasks
     with TaskGroup("data_transformation") as data_transformation:
         DIMENTSION_ITEM_TYPES = ["tracks", "artists", "podcasts", "episodes"]
         FACT_ITEM_TYPES = ["track", "podcast"]
         
+        # Populate core dims
         with TaskGroup("load_spotify_dims") as load_spotify_dims:
             for item_type in DIMENTSION_ITEM_TYPES:
                 load_dim = load_dims_task_groups(item_type)
@@ -185,23 +224,31 @@ with DAG("spotify_etl_dag", schedule_interval=None, catchup=False):
                     dim_stats[item_type]["total_time"] = total_time
                     dim_stats[item_type]["processed_count"] = processed_count
 
+            total_time = sum(stat.get("total_time", 0) for stat in dim_stats.values())
+            total_processed_count = sum(stat.get("processed_count", 0) for stat in dim_stats.values())
+
+            dim_stats["total_time"] = total_time
+            dim_stats["total_processed_count"] = total_processed_count
+
             logger.info(f"Spotify dims loaded: {dim_stats}")
             return dim_stats
         spoti_dim_stats = log_spoti_dims_load()
 
+        # Populate dim_reason table
         @task(trigger_rule="none_failed")
         def populate_dim_reason_task():
             db = PgConnectHook()
             logger = LoggingMixin().log
-            return populate_dim_reason(db, logger)
+            return transformer.populate_dim_reason(db, logger)
         run_populate_dim_reason = populate_dim_reason_task()
         
+        # Insert facts into core tables
         with TaskGroup("load_facts") as load_spotify_facts:
             @task
             def insert_core_facts_task(item_type):
                 db = PgConnectHook()
                 logger = LoggingMixin().log
-                return insert_core_facts(db, logger, item_type)
+                return transformer.insert_core_facts(db, logger, item_type)
             insert_track_facts = insert_core_facts_task.override(task_id="insert_track_facts")("track")
             insert_podcast_facts = insert_core_facts_task.override(task_id="insert_podcast_facts")("podcast")
 
@@ -222,8 +269,43 @@ with DAG("spotify_etl_dag", schedule_interval=None, catchup=False):
             insert_track_facts >> run_log_facts_load
             insert_podcast_facts >> run_log_facts_load
 
-        load_spotify_dims >> spoti_dim_stats >> run_populate_dim_reason >> load_spotify_facts
+        # Cleanup staging layer
+        @task
+        def cleanup_staging_task():
+            db = PgConnectHook()
+            logger = LoggingMixin().log
+            return transformer.cleanup_staging(db, logger)
+        run_cleanup_staging = cleanup_staging_task()
+
+        @task
+        def final_transformation_log_task(**context):
+            logger = LoggingMixin().log
+            ti = context["ti"]
+            dim_stats = ti.xcom_pull(task_ids="data_transformation.log_spoti_dims_load")
+            dim_reason_time = ti.xcom_pull(task_ids="data_transformation.populate_dim_reason_task")
+            facts_stats = ti.xcom_pull(task_ids="data_transformation.load_facts.log_facts_load")
+            cleanup_time = ti.xcom_pull(task_ids="data_transformation.cleanup_staging_task")
+
+            total_time = dim_stats["total_time"] + dim_reason_time + facts_stats["total_time"] + cleanup_time
+            logger.info(f"Data transformation completed in {total_time} seconds. Dims: {dim_stats}, Dim Reason Time: {dim_reason_time}, Facts: {facts_stats}, Cleanup Time: {cleanup_time}")
+
+            return total_time
+        final_transformation_log = final_transformation_log_task()
+
+        load_spotify_dims >> spoti_dim_stats >> run_populate_dim_reason >> load_spotify_facts >> run_cleanup_staging >> final_transformation_log
+
+    # final etl log
+    @task
+    def final_etl_log_task(**context):
+        logger = LoggingMixin().log
+        ti = context["ti"]
+        staging_time = ti.xcom_pull(task_ids="spotify_items_staging.log_staging_stats")["total_time"] or 0
+        transformation_time = ti.xcom_pull(task_ids="data_transformation.final_transformation_log_task")
+        
+        total_time = staging_time + transformation_time
+        logger.info(f"ETL process completed in {total_time} seconds. Extraction: {staging_time} seconds, Transformation: {transformation_time} seconds")
+    final_etl_log = final_etl_log_task()
 
     raw_files_extraction >> skip_staging_check >> [run_spotify_items_staging, run_transformation] 
     run_spotify_items_staging >> spotify_items_staging >> run_transformation
-    run_transformation >> data_transformation
+    run_transformation >> data_transformation >> final_etl_log
